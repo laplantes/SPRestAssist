c268aeffc1826992f5901f6cd4fd2dd362cd2275
Scott LaPlante <18195024+laplantes@users.noreply.github.com>
2021-02-17 14:54:25 -0500

    gulpfile.js - initial content commit

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp'),
+	rename = require('gulp-rename'),
+	soucermaps = require('gulp-sourcemaps'),
+	terser = require('gulp-terser'),
+	src = './src/sprestassist.js',
+	destination = './dist';
+
+
+const createDist = (source, destination) => {
+	return gulp.src(source)
+	.pipe(gulp.dest(destination))
+	.pipe(rename(function (path) {
+		path.basename += '.min';
+	}))
+	.pipe(soucermaps.init())
+	.pipe(terser())
+	.pipe(soucermaps.write('./'))
+	.pipe(gulp.dest(destination))
+};
+
+gulp.task('build', () => {
+	const files = createDist(src, destination);
+	return files;
+});
+
+// default task
+gulp.task('default', gulp.series('build'));
